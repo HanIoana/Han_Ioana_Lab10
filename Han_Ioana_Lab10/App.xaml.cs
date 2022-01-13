0028623146545267df598fb8bf959ad2cf135081
Ioana Bianca Han <Han.Io.Ioana@student.utcluj.ro>
2022-01-13 20:17:23 +0200

    lab10

---
+++ Han_Ioana_Lab10/App.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Han_Ioana_Lab10.Data;
+using System.IO;
+
+namespace Han_Ioana_Lab10
+{
+    public partial class App : Application
+    {
+        static ShoppingListDatabase database;
+        public static ShoppingListDatabase Database
+        {
+            get
+            {
+                if (database == null)
+                {
+                    database = new ShoppingListDatabase(Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.
+                    LocalApplicationData), "ShoppingList.db3"));
+                }
+                return database;
+            }
+        }
+
+        public App()
+        {
+            InitializeComponent();
+            MainPage = new NavigationPage(new ListEntryPage());
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
---
+++ Han_Ioana_Lab10/Models/ShopList.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using SQLite;
+
+
+namespace Han_Ioana_Lab10.Models
+{
+   public class ShopList
+    {
+        [PrimaryKey, AutoIncrement]
+        public int ID { get; set; }
+        public string Description { get; set; }
+        public DateTime Date { get; set; }
+    }
+    
+}
